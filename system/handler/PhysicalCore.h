4dcd4e2ded42f037c7764d01c3db156c2ebab1d2
Mohamed Belhadi <belhad.m@gmail.com>
2018-06-14 11:36:23 +0200

    [up] del space :(

--- system/handler/PhysicalCore.h
+++ system/handler/PhysicalCore.h
@@ -367,7 +367,7 @@ namespace cube {
             CPU_SET(_index, &cpuset);
 
 	    pthread_t current_thread = pthread_self();
-            ret = !pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
+	    ret = !pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
 #elif defined(_WIN32) || defined(_WIN64)
     #ifdef _MSC_VER
                 DWORD_PTR mask = (1 << (_index < 0 ? 0 : _index));
