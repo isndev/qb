93565f694fa3e5e305482608c5cc6b10638ace8b
Mohamed Belhadi <isn.dev@outlook.com>
2018-08-04 11:58:55 -0400

    [dev] added CoreSet hadnler, update pipe

--- system/start_sequence.h
+++ system/start_sequence.h
@@ -4,5 +4,6 @@
 # include "handler/PhysicalCore.h"
 # include "handler/TimedCore.h"
 # include "handler/CoreLinker.h"
+# include "handler/CoreSet.h"
 # include "handler/Engine.h"
 #endif //CUBE_START_SEQUENCE_H
