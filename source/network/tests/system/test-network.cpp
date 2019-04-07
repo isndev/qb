2ba1726036a5ac59a92c0d9d95c83dc92c3fba37
Mohamed Belhadi <belhad.m@gmail.com>
2019-04-07 18:56:54 +0200

    [dev] added new qb project/module script, clean cmake generation, improved qb-network coverage

--- source/network/tests/system/test-network.cpp
+++ source/network/tests/system/test-network.cpp
@@ -34,7 +34,7 @@ TEST(TCP, Blocking) {
         EXPECT_FALSE(listener.accept(sock) != qb::network::SocketStatus::Done);
         sock.setBlocking(true);
         char buffer[512];
-		*buffer = 0;
+        *buffer = 0;
         std::size_t received;
 
         EXPECT_FALSE(sock.receive(buffer, 512, received) != qb::network::SocketStatus::Done);
@@ -46,7 +46,7 @@ TEST(TCP, Blocking) {
         EXPECT_FALSE(sock.connect("127.0.0.1", port, 10) != qb::network::SocketStatus::Done);
         EXPECT_TRUE(sock.good());
         EXPECT_EQ(sock.getRemotePort(), port);
-		std::this_thread::sleep_for(std::chrono::seconds(3));
+        std::this_thread::sleep_for(std::chrono::seconds(3));
         const char msg[] = "Hello Test !";
         EXPECT_FALSE(sock.send(msg, sizeof(msg)) != qb::network::SocketStatus::Done);
         sock.disconnect();
@@ -57,36 +57,36 @@ TEST(TCP, Blocking) {
 }
 
 TEST(TCP, NonBlocking) {
-	std::thread tlistener([]() {
-		qb::network::tcp::Listener listener;
-		EXPECT_FALSE(listener.listen(port) != qb::network::SocketStatus::Done);
-		EXPECT_TRUE(listener.good());
+    std::thread tlistener([]() {
+        qb::network::tcp::Listener listener;
+        EXPECT_FALSE(listener.listen(port) != qb::network::SocketStatus::Done);
+        EXPECT_TRUE(listener.good());
         EXPECT_EQ(listener.getLocalPort(), port);
-		qb::network::tcp::Socket sock;
-		EXPECT_FALSE(listener.accept(sock) != qb::network::SocketStatus::Done);
-		sock.setBlocking(false);
-		char buffer[512];
-		*buffer = 0;
-		std::size_t received;
+        qb::network::tcp::Socket sock;
+        EXPECT_FALSE(listener.accept(sock) != qb::network::SocketStatus::Done);
+        sock.setBlocking(false);
+        char buffer[512];
+        *buffer = 0;
+        std::size_t received;
 
-		EXPECT_FALSE(sock.receive(buffer, 512, received) != qb::network::SocketStatus::NotReady);
-		EXPECT_EQ(std::string(buffer), "");
-	});
+        EXPECT_FALSE(sock.receive(buffer, 512, received) != qb::network::SocketStatus::NotReady);
+        EXPECT_EQ(std::string(buffer), "");
+    });
     std::this_thread::sleep_for(std::chrono::seconds(3));
-	std::thread tsender([]() {
-		qb::network::tcp::Socket sock;
-		EXPECT_FALSE(sock.connect("127.0.0.1", port, 10) != qb::network::SocketStatus::Done);
-		EXPECT_TRUE(sock.good());
+    std::thread tsender([]() {
+        qb::network::tcp::Socket sock;
+        EXPECT_FALSE(sock.connect("127.0.0.1", port, 10) != qb::network::SocketStatus::Done);
+        EXPECT_TRUE(sock.good());
         EXPECT_EQ(sock.getRemotePort(), port);
-		sock.setBlocking(false);
-		std::this_thread::sleep_for(std::chrono::seconds(3));
-		const char msg[] = "Hello Test !";
-		EXPECT_FALSE(sock.send(msg, sizeof(msg)) != qb::network::SocketStatus::Done);
-		sock.disconnect();
-	});
+        sock.setBlocking(false);
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        const char msg[] = "Hello Test !";
+        EXPECT_FALSE(sock.send(msg, sizeof(msg)) != qb::network::SocketStatus::Done);
+        sock.disconnect();
+    });
 
-	tlistener.join();
-	tsender.join();
+    tlistener.join();
+    tsender.join();
 }
 
 TEST(UDP, Blocking) {
@@ -96,7 +96,7 @@ TEST(UDP, Blocking) {
         EXPECT_TRUE(listener.good());
         EXPECT_EQ(listener.getLocalPort(), port);
         char buffer[512];
-		*buffer = 0;
+        *buffer = 0;
         std::size_t received;
 
         qb::network::ip from;
@@ -153,9 +153,15 @@ TEST(UDP, NonBlocking) {
 
 
 TEST(IP, Resolving) {
-  qb::network::ip ip("192.168.0.123");
-  std::cout << ip << std::endl;
-  EXPECT_EQ(ip.toString(), "192.168.0.123");
-  const std::string ip_str("8.8.8.8");
-  EXPECT_EQ(qb::network::ip(ip_str), qb::network::ip(8, 8, 8, 8));
+    EXPECT_EQ(qb::network::ip::Any, qb::network::ip(0, 0, 0, 0));
+    EXPECT_EQ(qb::network::ip::None, qb::network::ip("255.255.255.255"));
+    EXPECT_EQ(qb::network::ip::LocalHost, qb::network::ip(std::string("127.0.0.1")));
+    EXPECT_NE(qb::network::ip::None, qb::network::ip("google.com"));
+    EXPECT_GT(qb::network::ip::None, qb::network::ip::Any);
+    EXPECT_LT(qb::network::ip::Any, qb::network::ip::None);
+    EXPECT_GE(qb::network::ip::None, qb::network::ip::Any);
+    EXPECT_LE(qb::network::ip::Any, qb::network::ip::None);
+    qb::network::ip ip("192.168.0.123");
+    std::cout << ip << std::endl;
+    EXPECT_EQ(ip.toString(), "192.168.0.123");
 }
